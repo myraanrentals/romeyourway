fba54809fc80babee51a1abb013a287c22b2eef0
Kratagya-Webkorps <kratagya@webkorps.com>
2025-07-25 12:20:41 +0530

    updated cross icon size and disable inspect access

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,5 +13,24 @@ export class AppComponent {
   title = 'Travolo';
   onActivate(event: any) {
     window.scrollTo({ top: 0, behavior: 'smooth' });
+    document.addEventListener('contextmenu', (event) => event.preventDefault());
+    document.addEventListener('keydown', function (e) {
+      if (
+        e.key === 'F12' ||
+        (e.ctrlKey && e.shiftKey && ['I', 'C', 'J'].includes(e.key)) ||
+        (e.ctrlKey && e.key === 'U')
+      ) {
+        e.preventDefault();
+      }
+    });
+    setInterval(function () {
+      const before = new Date().getTime();
+      debugger;
+      const after = new Date().getTime();
+      if (after - before > 100) {
+        alert('Developer tools are open. Please close them to continue.');
+        window.location.href = '/';
+      }
+    }, 1000);
   }
 }

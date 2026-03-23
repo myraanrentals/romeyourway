00776fd8c4806eb04a013c0fd246734b342213c6
Kratagya-Webkorps <kratagya@webkorps.com>
2026-03-23 16:52:14 +0530

    updated code

--- src/app/constants/routes.ts
+++ src/app/constants/routes.ts
@@ -3,5 +3,5 @@ export const API_BASE_URL = 'https://myraanrentals.work/rentalcrm/';
 export const APIROUTES = {
   WATERSPORTSBOOKING: `${API_BASE_URL}registerLead`,
   PAYMENTSTATUS: `${API_BASE_URL}updatePaymentDetails`,
-  VALIDATECOUPON: `https://myraan.work/vehicle/validateCouponDetails`,
+  VALIDATECOUPON: `https://myraanrentals.work/vehicle/validateCouponDetails`,
 };
